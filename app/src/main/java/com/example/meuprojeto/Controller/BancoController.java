86a5a4f8ac5d530b26351ae56765a98b5fa51fc4
Talita Santos <TalitaRdSantos@outlook.com>
2018-01-17 01:32:50 -0300

    Initial commit

---
+++ app/src/main/java/com/example/meuprojeto/Controller/BancoController.java
@@ -0,0 +1,8 @@
+package com.example.meuprojeto.Controller;
+
+/**
+ * Created by Talit on 17/01/2018.
+ */
+
+public class BancoController {
+}
---
+++ app/src/main/java/com/example/meuprojeto/Model/Projeto.java
@@ -0,0 +1,45 @@
+package com.example.meuprojeto.Model;
+
+/**
+ * Created by Talit on 17/01/2018.
+ */
+
+public class Projeto {
+    long id;
+    private String nome;
+    private String descricao;
+    private String status;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+}
