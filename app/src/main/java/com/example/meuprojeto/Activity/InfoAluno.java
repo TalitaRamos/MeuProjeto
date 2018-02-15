9f1b03e34beadee7f01f5f72e2bf1ad7e273d185
Talita Ramos <talitasantosrd@gmail.com>
2018-02-15 00:05:17 -0200

    atualizando partes que não subiram

---
+++ app/src/main/java/com/example/meuprojeto/Activity/InfoAluno.java
@@ -0,0 +1,15 @@
+package com.example.meuprojeto.Activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.meuprojeto.R;
+
+public class InfoAluno extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_info_aluno);
+    }
+}
